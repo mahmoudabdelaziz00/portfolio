7a71d92aee727e0f2ff2003baea865c8e5d46a67
mahmoudabdelaziz00 <101192766+mahmoudabdelaziz00@users.noreply.github.com>
2024-05-24 23:22:52 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/**========= typing animation */
+
+var typed = new Typed(".typing", {
+    strings:["","Mobile Application Developer","Web Developer","Instructor"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
